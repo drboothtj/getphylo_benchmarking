01582b68c74ccfd37db4cd12f08df22eb2721298
DrBoothTJ <booth.thom@gmail.com>
2023-07-19 10:56:13 +0200

    comment and clean alignment information script

--- scripts/alignment_information.py
+++ scripts/alignment_information.py
@@ -1,33 +1,48 @@
-from Bio import AlignIO
-import sys
+'''
+A script for counting alignment size and informative characters.
 
-#read alignment
-filename = sys.argv[1] 
+Requirements:
+	Bio
+
+Usage:
+	python3 alignment_information.py myalignment.fasta
+'''
+import sys
+from Bio import AlignIO
+
+#1. Read the alignment from the file
+filename = sys.argv[1]
 alignment = AlignIO.read(filename, "fasta")
 
-
-#count informative sites
+#2. Count informative sites
 num_sequences = len(alignment)
 alignment_length = alignment.get_alignment_length()
 informative_sites = 0
 
+#For each column in the alignment...
 for i in range(alignment_length):
     characters = alignment[:, i]
+    #...discard gaps and missing data...
     characters = [
             character for character in characters if character not in ["X","-","?"]
             ]
-    if len(set(characters)) > 2: #is there more than 2 sites you can skip the later calculation
+    #...if there is more than one difference the site is informative...
+    if len(set(characters)) > 2:
         informative_sites += 1
-    elif len(set(characters)) > 1: #otherwise check that that the unique character appears more than once
+    #..if there are only two characters...
+    elif len(set(characters)) > 1:
         unique_characters = set(characters)
         characters_informative = []
         for character in unique_characters:
             count = characters.count(character)
             characters_informative.append(count > 2)
-            informative_characters = len([value for value in characters_informative if value == True])
+            informative_characters = len(
+                [value for value in characters_informative if value is True])
+            #...they are only informative if they appear more than once...
             if informative_characters > 1:
                 informative_sites +=1
 
+#3. Print the alignment information
+print('This is an alignment of %s sequences.' % num_sequences)
 print('The alignment length is %s.' % alignment_length)
 print('The number of informative sites is %s.' % informative_sites)
-
